2d452c2825fdeaf49c5ef001946dc65883fb7548
Tyagi <detyagi@deloitte.com>
2025-10-09 18:07:41 +0530

    updated code

---
+++ src/main/java/org/example/calculator/CalculatorController.java
@@ -0,0 +1,43 @@
+package org.example.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorController {
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.add(a, b);
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.subtract(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.divide(a, b);
+    }
+}
+package org.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+}
+
---
+++ src/main/java/org/example/calculator/CalculatorService.java
@@ -0,0 +1,21 @@
+package org.example.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return (double) a / b;
+    }
+}
+
